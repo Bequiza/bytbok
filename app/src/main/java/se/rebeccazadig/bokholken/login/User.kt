6a079ba6665ed49bf21e892723b496f7da1f7b42
Erkan Besalti <erkan.besalti@softhouse.se>
2023-08-10 13:46:27 +0200

    Forgot to add those changes

--- app/src/main/java/se/rebeccazadig/bokholken/login/User.kt
+++ app/src/main/java/se/rebeccazadig/bokholken/login/User.kt
@@ -1,9 +1,8 @@
 package se.rebeccazadig.bokholken.login
 
 data class User(
-
-    val id: String,
-    val name: String,
-    val contact: String,
-    val city: String,
+    val id: String = "",
+    val name: String = "",
+    val contact: String = "",
+    val city: String = ""
 )
